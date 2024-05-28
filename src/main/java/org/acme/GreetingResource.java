18860dbae0ea79be6fd6c049a4632f24efe01de7
Rushikesh Jadhav <jadhavrushikesh030@gmail.com>
2024-05-28 17:43:48 +0530

    Feedback Feature Application

---
+++ src/main/java/org/acme/GreetingResource.java
@@ -0,0 +1,19 @@
+package org.acme;
+
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import java.net.URI;
+import java.util.*;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello from Quarkus REST";
+    }
+}
